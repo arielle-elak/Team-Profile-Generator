2ef00352308b65dc208403e5f11c75d921957dec
Ari-chan <arielle.elak@gmail.com>
2022-10-15 22:42:16 -0600

    Realizing the classes are simply for writing to the HTML file, not for the inquirer questions. Migrated and tested from index only

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 // App dependencies
+const inquirer = require("inquirer");
+
 const Employee = require("./lib/Employee");
-const Manager = require('./lib/Manager');
+const Manager = require("./lib/Manager");
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-// Declare this a new instance of a new employee
-const employee = new Employee();
+// Start the team adding process
+function addTeam() {
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the Team Manager's Full Name?"
+            },
 
-// Start the program in lib/Employee.js
-employee.addTeam();
+            {
+                type: "input",
+                name: "managerID",
+                message: (answers) => `What is ${answers.managerName}'s Employee ID?`
+            },
+
+            {
+                type: "input",
+                name: "managerEmail",
+                message: (answers) => `What is ${answers.managerName}'s Email Address?`
+            },
+
+            {
+                type: "input",
+                name: "managerPhone",
+                message: (answers) => `What is ${answers.managerName}'s Phone Number?`
+            }
+        ])
+        .then((answers) =>
+            console.log(`${answers.managerName} | ${answers.managerID} | ${answers.managerEmail} | ${answers.managerPhone}`)
+        )
+};
+
+addTeam();
