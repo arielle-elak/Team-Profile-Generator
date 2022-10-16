de4534bfc550db1af4ce802aeebf341c81083bac
Ari-chan <arielle.elak@gmail.com>
2022-10-16 13:38:12 -0600

    For each iteration, the new card is being inserted into previous block. Need to generate as a separate card in own div

--- index.js
+++ index.js
@@ -188,20 +188,20 @@ function addMember(answers) {
 function createManager() {
     for (const Manager of managerArr) {
         return `
-<div class="col-12 col-sm-6 col-md-4 col-lg-4">
-    <div class="card teamCard">
-        <div class="card-header">
-            <h2>${Manager.name}</h2>
-            <h3>Manager</h3>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="card teamCard">
+                <div class="card-header">
+                    <h2>${Manager.name}</h2>
+                    <h3>Manager</h3>
+                </div>
+            <div class="outerGroup">
+                <ul class="list-group list-group-flush innerGroup">
+                    <li class="list-group-item">ID: ${Manager.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${Manager.email}">${Manager.email}</a></li>
+                    <li class="list-group-item">Office number: ${Manager.officeNumber}</li>
+                </ul>
+            </div>
         </div>
-    <div class="outerGroup">
-        <ul class="list-group list-group-flush innerGroup">
-            <li class="list-group-item">ID: ${Manager.id}</li>
-            <li class="list-group-item">Email: <a href="mailto:${Manager.email}">${Manager.email}</a></li>
-            <li class="list-group-item">Office number: ${Manager.officeNumber}</li>
-        </ul>
-    </div>
-</div>
 `
     };
 };
@@ -210,20 +210,20 @@ function createManager() {
 function createEngineer() {
     for (const Engineer of teamArr) {
         return `
-<div class="col-12 col-sm-6 col-md-4 col-lg-4">
-    <div class="card teamCard">
-        <div class="card-header">
-            <h2>${Engineer.name}</h2>
-            <h3>Engineer</h3>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="card teamCard">
+                <div class="card-header">
+                    <h2>${Engineer.name}</h2>
+                    <h3>Engineer</h3>
+                </div>
+            <div class="outerGroup">
+                <ul class="list-group list-group-flush innerGroup">
+                    <li class="list-group-item">ID: ${Engineer.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${Engineer.email}">${Engineer.email}</a></li>
+                    <li class="list-group-item">GitHub Username: ${Engineer.github}</li>
+                </ul>
+            </div>
         </div>
-    <div class="outerGroup">
-        <ul class="list-group list-group-flush innerGroup">
-            <li class="list-group-item">ID: ${Engineer.id}</li>
-            <li class="list-group-item">Email: <a href="mailto:${Engineer.email}">${Engineer.email}</a></li>
-            <li class="list-group-item">GitHub Username: ${Engineer.github}</li>
-         </ul>
-    </div>
-</div>
         `
     };
 };
@@ -232,20 +232,20 @@ function createEngineer() {
 function createIntern() {
     for (const Intern of teamArr) {
         return `
-<div class="col-12 col-sm-6 col-md-4 col-lg-4">
-    <div class="card teamCard">
-        <div class="card-header">
-            <h2>${Intern.name}</h2>
-            <h3>Engineer</h3>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="card teamCard">
+                <div class="card-header">
+                    <h2>${Intern.name}</h2>
+                    <h3>Engineer</h3>
+                </div>
+            <div class="outerGroup">
+                <ul class="list-group list-group-flush innerGroup">
+                    <li class="list-group-item">ID: ${Intern.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${Intern.email}">${Intern.email}</a></li>
+                    <li class="list-group-item">School: ${Intern.school}</li>
+                </ul>
+            </div>
         </div>
-    <div class="outerGroup">
-        <ul class="list-group list-group-flush innerGroup">
-            <li class="list-group-item">ID: ${Intern.id}</li>
-            <li class="list-group-item">Email: <a href="mailto:${Intern.email}">${Intern.email}</a></li>
-            <li class="list-group-item">School: ${Intern.school}</li>
-        </ul>
-    </div>
-</div>
 `
     };
 };
