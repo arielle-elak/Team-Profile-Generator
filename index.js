a77c800db470ee8d72819c8b646ab0cb4fda1e74
Ari-chan <arielle.elak@gmail.com>
2022-10-15 22:30:20 -0600

    First test of exporting Employee module to index

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const inquirer = require('inquier');
+// App dependencies
+const Employee = require("./lib/Employee");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// Declare this a new instance of a new employee
+const employee = new Employee();
+
+// Start the program in lib/Employee.js
+employee.addTeam();
