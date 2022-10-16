8c2aef0da01b17323a92555b66ed3149ee9419f7
Ari-chan <arielle.elak@gmail.com>
2022-10-16 12:13:38 -0600

    Changed HTML template layout to have manager in top row and all members in flex below to show hierarchy

--- index.js
+++ index.js
@@ -6,54 +6,155 @@ const Manager = require("./lib/Manager");
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+const createHTML = require('./src/template')
+
 // Arrays for capturing input during inquirer
 var managerArr = [];
 var memberArr = [];
 
-
-// Start the team adding process
+// Start the app and
+// Create new Manager object from Manager blueprint, taken from Employee master and push to manager array
 function startApp() {
-    return inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "managerName",
-                message: "What is the Team Manager's Full Name?"
-            },
+    inquirer.prompt(managerPrompts)
+    .then((answers) => {
+        const manager = new Manager(
+            answers.managerName,
+            answers.managerID,
+            answers.managerEmail,
+            answers.managerPhone
+        );
+        managerArr.push(manager);
+        console.log(manager);
+        console.log(managerArr);
+        askMember(answers);
+    });
+}
 
-            {
-                type: "input",
-                name: "managerID",
-                message: (answers) => `${answers.managerName}'s Employee ID:`
-            },
 
-            {
-                type: "input",
-                name: "managerEmail",
-                message: (answers) => `${answers.managerName}'s Email Address:`
-            },
+// Create new Engineer object from Manager blueprint, taken from Employee master and push to member array
+function addEngineer() {
+    inquirer.prompt(engineerPrompts)
+    .then((answers) => {
+        const engineer = new Engineer(
+            answers.engName,
+            answers.engID,
+            answers.engEmail,
+            answers.engGit
+        );
+        memberArr.push(engineer);
+        console.log(engineer);
+        console.log(memberArr);
+        askMember(answers);
+    });
+}
 
-            {
-                type: "input",
-                name: "managerPhone",
-                message: (answers) => `${answers.managerName}'s Phone Number:`
-            }
+// Create new Intern object from Manager blueprint, taken from Employee master and push to member array
+function addIntern() {
+    inquirer.prompt(internPrompts)
+    .then((answers) => {
+        const intern = new Intern(
+            answers.intName,
+            answers.intID,
+            answers.intEmail,
+            answers.intSchool
+        );
+        memberArr.push(intern);
+        console.log(intern);
+        console.log(memberArr);
+        askMember(answers);
+    });
+}
 
-        ])
-        .then((answers) => {
-            // Create new Manager object from Manager blueprint, taken from Employee master and push to manager array
-            const manager = new Manager(
-                answers.managerName,
-                answers.managerID,
-                answers.managerEmail,
-                answers.managerPhone
-            );
-            managerArr.push(manager);
-            console.log(manager);
-            console.log(managerArr);
-            askMember(answers);
-        });
+// Finish team and generate the HTML based on the array responses
+function finishTeam() {
+    console.log("Your team is Complete! Please see the dist/ directory for your finished HTML file.")
+    createHTML();
 };
+
+
+// Prompts to ask for Manager
+const managerPrompts = [
+    {
+        type: "input",
+        name: "managerName",
+        message: "What is the Team Manager's Full Name?"
+    },
+
+    {
+        type: "input",
+        name: "managerID",
+        message: (answers) => `${answers.managerName}'s Employee ID:`
+    },
+
+    {
+        type: "input",
+        name: "managerEmail",
+        message: (answers) => `${answers.managerName}'s Email Address:`
+    },
+
+    {
+        type: "input",
+        name: "managerPhone",
+        message: (answers) => `${answers.managerName}'s Phone Number:`
+    }
+
+];
+
+// Prompts to ask for Engineer
+const engineerPrompts = [
+    {
+        type: "input",
+        name: "engName",
+        message: "What is the Engineer's Name?"
+    },
+
+    {
+        type: "input",
+        name: "engID",
+        message: (answers) => `${answers.engName}'s Employee ID:`
+    },
+
+    {
+        type: "input",
+        name: "engEmail",
+        message: (answers) => `${answers.engName}'s Email Address:`
+    },
+
+    {
+        type: "input",
+        name: "engGit",
+        message: (answers) => `${answers.engName}'s GitHub Username:`
+    }
+];
+
+// Prompts to ask for Intern
+const internPrompts = [
+    {
+        type: "input",
+        name: "intName",
+        message: "What is the Intern's Name?"
+    },
+
+    {
+        type: "input",
+        name: "intID",
+        message: (answers) => `${answers.intName}'s Employee ID:`
+    },
+
+    {
+        type: "input",
+        name: "intEmail",
+        message: (answers) => `${answers.intName}'s Email Address:`
+    },
+
+    {
+        type: "input",
+        name: "intSchool",
+        message: (answers) => `${answers.intName}'s GitHub Username:`
+    }
+];
+
+
 
 // Start the team adding process
 function askMember(answers) {
@@ -85,79 +186,7 @@ function addMember(answers) {
     };
 };
 
-function addEngineer() {
-    //console.log("Chose to add an Engineer")
-    return inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "engName",
-                message: "What is the Engineer's Name?"
-            },
 
-            {
-                type: "input",
-                name: "engID",
-                message: (answers) => `${answers.engName}'s Employee ID:`
-            },
-
-            {
-                type: "input",
-                name: "engEmail",
-                message: (answers) => `${answers.engName}'s Email Address:`
-            },
-
-            {
-                type: "input",
-                name: "engGit",
-                message: (answers) => `${answers.engName}'s GitHub Username:`
-            }
-
-        ])
-        .then((answers) =>
-            console.log(`${answers.engName} | ${answers.engID} | ${answers.engEmail} | ${answers.engGit}` )
-    )
-
-};
-
-function addIntern() {
-    //console.log("Chose to add an Intern")
-    return inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "intName",
-                message: "What is the Intern's Name?"
-            },
-
-            {
-                type: "input",
-                name: "intID",
-                message: (answers) => `${answers.intName}'s Employee ID:`
-            },
-
-            {
-                type: "input",
-                name: "intEmail",
-                message: (answers) => `${answers.intName}'s Email Address:`
-            },
-
-            {
-                type: "input",
-                name: "intSchool",
-                message: (answers) => `${answers.intName}'s GitHub Username:`
-            }
-
-        ])
-        .then((answers) =>
-            console.log(`${answers.intName} | ${answers.intID} | ${answers.intEmail} | ${answers.intSchool}` )
-    )
-
-};
-
-function finishTeam() {
-    console.log("Your team is Complete! Please see the dist/ directory for your finished HTML file.")
-};
 
 
 
---
+++ src/template.js
@@ -0,0 +1,133 @@
+// Template for HTML Generation
+
+function createHTML() {
+
+    return`<!--Team Profile Generator HTML-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--Boostrap and Responsive Viewport-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>My Team</title>
+</head>
+<body>
+    <header>
+        <h1>My Team</h1>
+    </header>
+
+    <section class="container-fluid row managerCard">
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="card teamCard">
+                <div class="card-header">
+                    <h2>Jared</h2>
+                    <h3>Manager</h3>
+                </div>
+            <div class="outerGroup">
+                <ul class="list-group list-group-flush innerGroup">
+                    <li class="list-group-item">ID: 1</li>
+                    <li class="list-group-item">Email: <a href="mailto:jared@fakemail.com">jared@fakemail.com</a></li>
+                     <li class="list-group-item">Office number: 123-456-7890</li>
+                </ul>
+            </div>
+        </div>
+
+    </section>
+
+
+
+    <section class="container-fluid row teamCardGroup">
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="card teamCard">
+                <div class="card-header">
+                    <h2>Guy</h2>
+                    <h3>Engineer</h3>
+                </div>
+                <div class="outerGroup">
+                    <ul class="list-group list-group-flush innerGroup">
+                        <li class="list-group-item">ID: 1</li>
+                        <li class="list-group-item">Email: <a href="mailto:guy@fakemail.com">guy@fakemail.com</a></li>
+                        <li class="list-group-item">GitHub Username: <a href="https://github.com/some-guy/">some-guy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="card teamCard">
+                <div class="card-header">
+                    <h2>Guy</h2>
+                    <h3>Engineer</h3>
+                </div>
+                <div class="outerGroup">
+                    <ul class="list-group list-group-flush innerGroup">
+                        <li class="list-group-item">ID: 1</li>
+                        <li class="list-group-item">Email: <a href="mailto:guy@fakemail.com">guy@fakemail.com</a></li>
+                        <li class="list-group-item">GitHub Username: <a href="https://github.com/some-guy/">some-guy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="card teamCard">
+                <div class="card-header">
+                    <h2>Guy</h2>
+                    <h3>Engineer</h3>
+                </div>
+                <div class="outerGroup">
+                    <ul class="list-group list-group-flush innerGroup">
+                        <li class="list-group-item">ID: 1</li>
+                        <li class="list-group-item">Email: <a href="mailto:guy@fakemail.com">guy@fakemail.com</a></li>
+                        <li class="list-group-item">GitHub Username: <a href="https://github.com/some-guy/">some-guy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="card teamCard">
+                <div class="card-header">
+                    <h2>Guy</h2>
+                    <h3>Engineer</h3>
+                </div>
+                <div class="outerGroup">
+                    <ul class="list-group list-group-flush innerGroup">
+                        <li class="list-group-item">ID: 1</li>
+                        <li class="list-group-item">Email: <a href="mailto:guy@fakemail.com">guy@fakemail.com</a></li>
+                        <li class="list-group-item">GitHub Username: <a href="https://github.com/some-guy/">some-guy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+            <div class="card teamCard">
+                <div class="card-header">
+                    <h2>Guy</h2>
+                    <h3>Engineer</h3>
+                </div>
+                <div class="outerGroup">
+                    <ul class="list-group list-group-flush innerGroup">
+                        <li class="list-group-item">ID: 1</li>
+                        <li class="list-group-item">Email: <a href="mailto:guy@fakemail.com">guy@fakemail.com</a></li>
+                        <li class="list-group-item">GitHub Username: <a href="https://github.com/some-guy/">some-guy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+</body>
+</html>
+`
+};
+
+module.exports = createHTML
