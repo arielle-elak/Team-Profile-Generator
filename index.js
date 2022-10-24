42128c7a64f0e505be59cbec04e3320bd566832f
arielle-elak <arielle.elak@gmail.com>
2022-10-24 16:13:55 -0600

    Further converting into objects, performing html content merge before final implementation into export

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inquirer = require("inquirer");
 const path = require('path');
 const fs = require('fs');
 
-const Employee = require("./lib/Employee");
+
 const Manager = require("./lib/Manager");
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
--- src/template.js
+++ src/template.js
@@ -2,37 +2,10 @@
 
 // Cycle through each Array
 
-const generateHTML = renderHTML => {
-
-function generateManager(managerArr) {
-    if (managerArr.length > 0) {
-        for (const manager of managerArr) {
-            createManager(manager);
-        };
-    };
-};
-
-
-function generateEngineer(engineerArr) {
-    if (engineerArr.length > 0) {
-        for (const engineer of engineerArr) {
-            createEngineer(engineer);
-        };
-    };
-};
-
-
-function generateIntern(internArr) {
-    if (internArr.length > 0) {
-        for (const intern of internArr) {
-            createIntern(intern);
-        };
-    };
-};
+const generateHTML = pageContent => {
 
 // Create the Manager HTML content based on user input using Manager class blueprint
-
-function createManager(manager) {
+const createManager = manager => {
         console.log("Generated Manager");
         return `
         <div class="col-12 col-sm-6 col-md-4 col-lg-4">
@@ -49,11 +22,11 @@ function createManager(manager) {
                 </ul>
             </div>
         </div>
-`
+`;
 };
 
 // Create the Engineer HTML content based on user input using Engineer class blueprint
-function createEngineer(engineer) {
+const createEngineer = engineer => {
         console.log("Generated Engineer");
         return `
         <div class="col-12 col-sm-6 col-md-4 col-lg-4">
@@ -70,14 +43,12 @@ function createEngineer(engineer) {
                 </ul>
             </div>
         </div>
-`
+`;
 };
 
 
 // Create the Intern HTML content based on user input using Intern class blueprint
-
-
-function createIntern(intern) {
+const createIntern = intern => {
         console.log("Generated Intern");
         return `
         <div class="col-12 col-sm-6 col-md-4 col-lg-4">
@@ -94,44 +65,43 @@ function createIntern(intern) {
                 </ul>
             </div>
         </div>
-`
+`;
 };
 
-// Write the HTML content
-function generateHTML(managerArr, engineerArr, internArr) {
+const innerContent = [];
 
-    return`<!--Team Profile Generator HTML-->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!--Boostrap and Responsive Viewport-->
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>My Team</title>
-</head>
-<body>
-    <header>
-        <h1>My Team</h1>
-    </header>
+managerArr.forEach(manager => {
+    innerContent.push(createManager(manager));
+});
+
+engineerArr.forEach(engineer => {
+    innerContent.push(createEngineer(engineer));
+});
+
+internArr.forEach(intern => {
+    innerContent.push(createIntern(intern));
+});
+
+return innerContent.join("");
+
+};
+
+
+// Write the HTML content
+module.exports = pageContent => {
+    return `
     <section class="container-fluid row managerCard">
         ${generateManager(managerArr)}
     </section>
+
     <section class="container-fluid row teamCardGroup">
         ${generateEngineer(engineerArr)}
     </section>
+
     <section class="container-fluid row teamCardGroup">
         ${generateIntern(internArr)}
     </section>
-</body>
-</html>
-`
-};
+    `;
 };
 
 
-module.exports = renderHTML => {
-    generateHTML();
-}
